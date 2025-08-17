3075a9e811e8b2f99394c054e88998e3387b4dc9
aroramrinaal <apply.mrinaal@gmail.com>
2025-08-17 14:50:04 +0530

    Update dependencies and enhance layout with SEO and analytics integration; remove unused SVG files.

---
+++ src/app/components/seo.tsx
@@ -0,0 +1,41 @@
+export default function SEO() {
+    return (
+      <>
+        {/* Primary Meta Tags */}
+        <title></title>
+        <meta
+          name="title"
+          content=""
+        />
+        <meta
+          name="description"
+          content=""
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+  
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="" />
+        <meta
+          property="og:description"
+          content=""
+        />
+        <meta property="og:image" content="" />
+  
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta name="twitter:url" content="" />
+        <meta name="twitter:title" content="" />
+        <meta
+          name="twitter:description"
+          content=""
+        />
+        <meta name="twitter:image" content="" />
+      </>
+    );
+  }
